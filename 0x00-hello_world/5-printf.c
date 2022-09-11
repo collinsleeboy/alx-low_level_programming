6dd6d40ad6fedf8e9a717c390fad026419787315
collinsleeboy <liboyic@gmail.com>
2022-09-11 06:35:45 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,26 +1,10 @@
-#include <stdio.h>                                                                                                                        
-
-                                                                                                                                          
-
-/**                                                                                                                                       
-
-*program that prints exactly with proper grammar                                                                                          
-
-*                                                                                                                                         
-
-*but the outcome is a piece of art                                                                                                        
-
-*Use the function printf                                                                                                                  
-
-*/                                                                                                                                        
-
-int main(void)                                                                                                                            
-
-{                                                                                                                                         
-
-printf("with proper grammar, but the outcome is a pi                                                                                      
-
-ece of art,\n");                                                                                                                          
-
-return (0);                                                                                                                               
-
+include <stdio.h>
+/**
+*print main value
+*
+*/
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\");
+return (0);
+}
