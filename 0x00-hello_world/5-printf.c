0ea431894d48174c56e060d5af4d8765fcc7b640
collinsleeboy <liboyic@gmail.com>
2022-09-11 06:52:38 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,13 @@
-include <stdio.h>
+#include <stdio.h>
 /**
-*print main value
+* main - print string function
 *
+*Description: using the main function
+*print programming
+*Return :0
 */
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,\");
+printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
