bcf930577f97e4033559f060fd98341dc875be80
collinsleeboy <liboyic@gmail.com>
2022-09-11 06:17:08 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,26 @@
+#include <stdio.h>                                                                                                                        
+
+                                                                                                                                          
+
+/**                                                                                                                                       
+
+*program that prints exactly with proper grammar                                                                                          
+
+*                                                                                                                                         
+
+*but the outcome is a piece of art                                                                                                        
+
+*Use the function printf                                                                                                                  
+
+*/                                                                                                                                        
+
+int main(void)                                                                                                                            
+
+{                                                                                                                                         
+
+printf("with proper grammar, but the outcome is a pi                                                                                      
+
+ece of art,\n");                                                                                                                          
+
+return (0);                                                                                                                               
+
