7b13a9bae956d57e7b470130ec8ce11ef91456a5
collinsleeboy <liboyic@gmail.com>
2022-09-12 14:13:39 +0300

    task 01

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -26,14 +26,14 @@ printf("%n is zero\n", n);
 if (n > 0)
 >>>>>>> 4043f50ff6c4d608ff96ddd35e30f7080698521e
 {
-   printf("i is positive\n", n);
+   printf(%i is positive\n", n);
 }
 else if (n == 0)
 {
-   printf("i is zero\n", n);
+   printf(%i is zero\n", n);
 }
 else if (n < 0)
 {
-  printf("i is negative\n", n);
+  printf(%i is negative\n", n);
 }
 return (0);
