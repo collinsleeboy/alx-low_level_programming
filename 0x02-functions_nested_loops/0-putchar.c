b6eaa619ff51c63cfbd279f821b41786f066aa48
collinsleeboy <liboyic@gmail.com>
2022-09-13 09:11:30 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+
+
+/**
+
+ * main - check the code
+
+ *
+
+ * Return: Always 0.
+
+ */
+int main(void)
+
+{
+
+	char *sh = "Holberton";
+
+
+
+	while (*sh)
+
+	{
+
+		_putchar(*sh);
+
+		sh++;
+
+	}
+
+	_putchar('\n');
+
+
+
+	return (0);
+
+}
