d4324b2980e62058652764f7b33fadf08968a5d7
collinsleeboy <liboyic@gmail.com>
2022-09-28 19:29:55 +0300

    task 2

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,22 +1,15 @@
 #include "main.h"
 
 /**
-
- * _strlen_recursion - Returns Length of String
-
- * @s: string
-
- *
-
- * Return: On success 1.
-
- * On error, -1 is returned, and errno is set appropriately.
-
- */
+* _strlen_recursion - Returns Length of String
+* @s: string
+*
+* Return: On success 1.
+* On error, -1 is returned, and errno is set appropriately.
+*/
 int _strlen_recursion(char *s)
-
 {
-int sun = 0;
+  int sun = 0;
 if (*s != '\0')
 {
 sum++;
