eb67734501f3ebe918f497c21a552eaad8863618
collinsleeboy <liboyic@gmail.com>
2022-09-28 19:01:51 +0300

    d

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -8,12 +8,12 @@
 */
 void _puts_recursion(char *s)
 {
-      if(*s == '\0')
-      {
+      if (*s == '\0')
+       {
               _putchar('\n');
-      }
-      else
-      {
+       }
+       else
+       {
               _putchar(s[0]);
               _puts_recursion(s + 1);
       }
