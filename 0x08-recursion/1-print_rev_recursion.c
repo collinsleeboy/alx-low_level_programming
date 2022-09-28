c33df879ca929557a4ee71aafe2c44baf8780299
collinsleeboy <liboyic@gmail.com>
2022-09-28 19:15:10 +0300

    task 1

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -2,15 +2,10 @@
 
 /**
 * _print_rev_recursion - Prints a string in reverse
-
 * @s: string
-
 *
-
 * Return: On success 1.
-
 * On error, -1 is returned, and errno is set appropriately.
-
 */
 void _print_rev_recursion(char *s)
 {
