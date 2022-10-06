76e5fe957854657400b7bf0175f47862b29c1b61
collinsleeboy <liboyic@gmail.com>
2022-10-07 01:58:31 +0300

    task 0

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,18 +1,21 @@
 #include "holberton.h"
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
+
 /**
-* malloc_checked -  allocates memory using malloc.
-* @b: size
-*
-* Return: Pointer to allocated memory or normal process termination
-* with a status value of 98
-*/
+  * malloc_checked - Allocates memory
+  * @b: the size to allocate
+  *
+  * Return: Nothing.
+  */
 void *malloc_checked(unsigned int b)
 {
 	void *p;
+
 	p = malloc(b);
-	if (p == 0)
+
+	if (p == NULL)
 		exit(98);
+
 	return (p);
 }
